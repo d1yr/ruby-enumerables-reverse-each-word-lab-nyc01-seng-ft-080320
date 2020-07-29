6be919e41ab6e24e9e056279a8ca9ab605faec9d
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 06:02:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(string)
   original = string.split("")
   new_array = []
   original.each do |string|
-    new_array << string.reverse
+    new_array << string.reverse!
   end
   new_array.join("")
+  new_array
 end
