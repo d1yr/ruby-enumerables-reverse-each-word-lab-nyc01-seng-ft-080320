8eca452c544c955f2d6c21831665a2c0d9574c1f
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 06:05:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,8 @@
 def reverse_each_word(string)
-#  original = string.split("")
-#  new_array = []
-#  original.each do |string|
-#    new_array << string.reverse
-#  end
-#  new_array.join("")
-#end
-#
-original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
+  original = string.split(" ")
+  new_array = []
+  original.each do |string|
+    new_array << string.reverse
   end
-  return_array.join(" ")
+ new_array.join(" ")
 end
